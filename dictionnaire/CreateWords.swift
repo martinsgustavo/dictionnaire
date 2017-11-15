5a0ae10868a2f2c1f547f83413b07ffdd3b93f7d
Gustavo Martins <lgustavo88@hotmail.com>
2017-11-15 13:24:04 -0500

    v3 - with constraints

--- dictionnaire/CreateWords.swift
+++ dictionnaire/CreateWords.swift
@@ -1,23 +1,30 @@
-
+//-----
 import UIKit
-
+//-----
 class CreateWords: UIViewController, UITextFieldDelegate{
+    //Les connexions
     @IBOutlet weak var subject: UITextField!
     @IBOutlet weak var wordFrench: UITextField!
     @IBOutlet weak var wordEnglish: UITextField!
-    
+    //Variable pour le sujet
     var subjText = ""
+    //Variables pour les tableaux dans dictionnaire
     var arrFrench = [String]()
     var arrEnglish = [String]()
+    //Constantes pour garder le dictionnaire et le sujet
     let defObjFrench = UserDefaultsManager()
     let defObjEnglish = UserDefaultsManager()
     let subjectMem = UserDefaultsManager()
     
+//-----
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Initialiser avec des mots écrits en mémoire
         manageUserDef()
         subject.text = subjText
     }
+//-----
+    //Bouton pour enregistrer les nouveaux mots dans les tableaux
     @IBAction func createDictionnary(_ sender: UIButton) {
         if wordFrench.text != "" && wordEnglish.text != "" {
         arrFrench.append(wordFrench.text!)
@@ -27,23 +34,28 @@ class CreateWords: UIViewController, UITextFieldDelegate{
         }
         cleanerFields()
     }
+//-----
+    //Vider les champs
     func cleanerFields() {
         wordFrench.text = ""
         wordEnglish.text = ""
     }
-    
+//-----
+    //Fonction pour fermer le clavier
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
+//-----
+    //Fonction pour garder le sujet dans le mémoire
     func textFieldDidEndEditing(_ textField: UITextField) {
         if subject.text != "" {
             subjText = subject.text!
             subjectMem.setKey(theValue: subjText as AnyObject, theKey: "subject")
         }
     }
-    
+//-----
+    //Fonction pour capturer des informations stockées dans la mémoire
     func manageUserDef() {
         if defObjFrench.doesKeyExist(theKey: "french") {
             arrFrench = defObjFrench.getValue(theKey: "french") as! [String]
@@ -57,4 +69,6 @@ class CreateWords: UIViewController, UITextFieldDelegate{
             subjText = subjectMem.getValue(theKey: "subject") as! String
         }
     }
+//-----
 }
+//-----
--- dictionnaire/ViewController.swift
+++ dictionnaire/ViewController.swift
@@ -1,31 +1,37 @@
-
+//----
 import UIKit
-
+//----
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //Les connexions
     @IBOutlet weak var subjTittle: UILabel!
     @IBOutlet weak var tabView: UITableView!
     @IBOutlet weak var translation: UILabel!
-    
+    //Variables pour le dictionnaire
     var dictOfWords = [String: String]()
+    //Variables pour les tableaux dans dictionnaire
     var arrOfFrench = [String]()
     var arrOfEnglish = [String]()
-    
+    //Variable pour le sujet
     var subjText = ""
+    //Constantes pour garder le dictionnaire et le sujet
     let defObjFrench = UserDefaultsManager()
     let defObjEnglish = UserDefaultsManager()
     let subjectMem = UserDefaultsManager()
-    
+//----
     override func viewDidLoad() {
         super.viewDidLoad()
-//        defObjFrench.removeKey(theKey: "french")
-//        defObjFrench.removeKey(theKey: "english")
+        //Initialiser avec des mots écrits en mémoire
         manageUserDef()
         translation.text! = "cliquez sur le mot pour voir la traduction"
         translation.textColor = UIColor.gray
         subjTittle.text = subjText
+        //Créer un dictionnaire
         dictOfWords = Dictionary (uniqueKeysWithValues: zip(arrOfFrench, arrOfEnglish))
+        //Rafraîchir le tableView
         tabView.reloadData()
     }
+//----
+    //Action pour le choix de la langue
     @IBAction func chooseLanguage(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             dictOfWords = Dictionary (uniqueKeysWithValues: zip(arrOfFrench, arrOfEnglish))
@@ -41,25 +47,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             sender.setTitle("English", forSegmentAt: 1)
         }
         tabView.reloadData()
-        
     }
-    
+//----
+    //Fonction pour compter les éléments dans tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return dictOfWords.count
     }
-
+//----
+    //Fonction pour afficher les mots dans tableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell (style: UITableViewCellStyle.default, reuseIdentifier: nil)
         cell.textLabel?.text = [String](dictOfWords.keys)[indexPath.row]
         return cell
     }
-    
+//----
+    //Fonction pour afficher les mots traduits dans le Label
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         translation.text = [String](dictOfWords.values)[indexPath.row]
         translation.textColor = UIColor.black
     }
-    
+//----
+    //Fonction pour capturer des informations stockées dans la mémoire
     func manageUserDef() {
         if defObjFrench.doesKeyExist(theKey: "french") {
             arrOfFrench = defObjFrench.getValue(theKey: "french") as! [String]
@@ -73,23 +81,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             subjText = subjectMem.getValue(theKey: "subject") as! String
         }
     }
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == UITableViewCellEditingStyle.delete {
-//            let cellDelete: String = tableView.cellForRow(at: indexPath)!.textLabel!.text!
-//            let arrayDelete = cellDelete.split(separator: " ")
-//            for i in arrayDelete {
-//                if let index = dictOfWords.index(forKey: String(i)) {
-//                    dictOfWords.remove(at: index)
-//                    memory.setKey(theValue: dictOfWords as AnyObject, key: "Dictionnarie")
-//                }
-//            }
-//        }
-//        tabView.reloadData()
-//    }
 
 }
-
-/// delete rows fazer isso
 
 
 
